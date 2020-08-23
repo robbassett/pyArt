7ea438f9c9a3f5520f96f156a56e4d465e3b292c
Robert Bassett <robertbassett@Roberts-MacBook-Pro.local>
2020-08-23 20:46:42 +1000

    phonics for donkeys

--- art_objs.py
+++ art_objs.py
@@ -1,6 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def rotate_coords(crds,ang):
+    rmat = np.array([[np.cos(ang),(-1.)*np.sin(ang)],
+                         [np.sin(ang),np.cos(ang)]])
+
+    return np.dot(crds.T,rmat).T
+
 class SimpleLine(object):
 
     def __init__(self,x1,x2,y1,y2):
@@ -157,6 +163,7 @@ class PowerElower(object):
         ty.append(ty[0])
         self.values[f'layer{self.layer}']['cents'] = np.vstack((tx,ty))
 
+# WORK N PROGRESS
 class SpinZoomer():
 
     def __init__(self,N=5,ang=15.*(np.pi/180.)):
@@ -289,12 +296,71 @@ class FractalFlower(object):
                     ax.plot(tmd[i][0],tmd[i][1],'-',c=tmc,lw=tmlw,alpha=.1)
                     ax.plot(tmd[i][0]*lfct,tmd[i][1]*lfct,'-',c=tmc,lw=tmlw,alpha=.2)
                     ax.plot(tmd[i][0]*hfct,tmd[i][1]*hfct,'-',c=tmc,lw=tmlw,alpha=.2)
-        
-        
 
+class Pyrograph():
+
+    def __init__(self,hole=0.1,inner_frac=0.636,outer_R = 1.,xo=0.,yo=0.,nps=1000):
+        self.npoints = nps
+        self.R = outer_R
+        self.r = self.R*inner_frac
+        self.rho = self.r*hole
+
+        dm = divmod(self.R,(1.-self.r))
+        self.one_orb = 2.*np.pi*(dm[0]-1)
+        if self.one_orb == 0.:
+            self.one_orb = 2.*np.pi
+        self.theta = 0.
+        self.cx,self.cy = 0.,0.
+
+        self.n = 0
+
+    def one_orbit(self):
+
+        theta = np.linspace(float(self.n)*self.one_orb,float(self.n+1)*self.one_orb,self.npoints)
+        cx = (self.R-self.r)*np.cos(theta)
+        cy = (self.R-self.r)*np.sin(theta)
+        itheta = ((-1.)*(self.R-self.r)/self.r)*theta
+        ix = self.rho*np.cos(itheta)
+        iy = self.rho*np.sin(itheta)
+
+        if self.n == 0.:
+            self.x = cx+ix
+            self.y = cy+iy
+            self.n += 1
+        else:
+            self.x = np.vstack((self.x,cx+ix))
+            self.y = np.vstack((self.y,cy+iy))
+            self.n += 1
+
+def testing():
+    from color_tools import colorFader as cF
+    
+    F = plt.figure(frameon=False,dpi=150)
+    F.set_size_inches(3,3)
+    ax = plt.Axes(F,[0.,0.,1.,1.])
+    ax.set_axis_off()
+    ax.set_aspect('equal')
+    F.add_axes(ax)
+
+    norb = 175
+    c1 = 'm'
+    c2 = 'darkcyan'
+    test = Pyrograph()
+    t2 = Pyrograph(hole=0.63,inner_frac=0.635,outer_R = .38)
+    for i in range(norb):
+        test.one_orbit()
+        t2.one_orbit()
+    for i in range(norb):
+        col=cF(c1,c2,float(i)/float(norb-1))
+        c22 = cF('limegreen','tab:orange',float(i)/float(norb-1))
+        ax.plot(test.x[i],test.y[i],'-',c=col,lw=2,alpha=1)
+        ax.plot(t2.x[i],t2.y[i],'-',c=c22,alpha=.6,lw=1)
+    plt.show()
+                    
 if __name__ == '__main__':
 
+    """
     sz=SpinZoomer()
     for i in range(2): sz.add_one()
     sz.tmp()
-    
+    """

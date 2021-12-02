ec2fc80d70e5ffc10b1aca1ecafb034bbff7e23f
Rob Bassett <33643965+robbassett@users.noreply.github.com>
2021-12-02 11:43:39 +1100

    subpenrosed

--- tiling/penrose.py
+++ tiling/penrose.py
@@ -3,32 +3,28 @@ from matplotlib.path import Path
 import matplotlib.patches as patches
 import numpy as np
 
-class Kite():
+class P2_base():
 
     def __init__(self,point,angle,l):
-        self.type='Kite'
         self.L = l
-        a1 = (np.pi/180.)*36.
-        phi = l*(1.+np.sqrt(5))/2.
+        self.angle = angle
+        self.point = point
+        self.a1 = (np.pi/180.)*36.
+        self.phi = l*(1.+np.sqrt(5))/2.
 
-        xs = [0,l*np.cos(a1),l,l*np.cos(a1),0]
-        ys = [0,l*np.sin(a1),0,-l*np.sin(a1),0]
-
+    def _make_path(self):
         rmat = np.array([
-            [np.cos(angle),(-1.)*np.sin(angle)],
-            [np.sin(angle),np.cos(angle)]
+            [np.cos(self.angle),(-1.)*np.sin(self.angle)],
+            [np.sin(self.angle),np.cos(self.angle)]
         ])
 
-        vs = np.vstack((xs,ys))
+        vs = np.vstack((self.xs,self.ys))
         vs = np.dot(vs.T,rmat).T
-        vs[0]+=point[0]
-        vs[1]+=point[1]
-        self.verts = [(vs[0][i],vs[1][i]) for i in range(len(xs))]
-        self.angle = angle
-        self.a1 = a1
-        self.phi = phi
+        vs[0]+=self.point[0]
+        vs[1]+=self.point[1]
+        self.verts = [(vs[0][i],vs[1][i]) for i in range(len(self.xs))]
 
-        self.codes = [
+        codes = [
             Path.MOVETO,
             Path.LINETO,
             Path.LINETO,
@@ -36,7 +32,16 @@ class Kite():
             Path.CLOSEPOLY,
         ]
 
-        self.path = Path(self.verts,self.codes)
+        self.path = Path(self.verts,codes)
+
+class Kite(P2_base):
+
+    def __init__(self,point,angle,l):
+        super().__init__(point,angle,l)
+        self.type='Kite'
+        self.xs = [0,l*np.cos(self.a1),l,l*np.cos(self.a1),0]
+        self.ys = [0,l*np.sin(self.a1),0,-l*np.sin(self.a1),0]
+        self._make_path()
 
     def inflate(self):
         l = self.L - self.phi
@@ -47,40 +52,14 @@ class Kite():
 
         return k1,k2,d1,d2
 
-class Dart():
+class Dart(P2_base):
 
     def __init__(self,point,angle,l):
+        super().__init__(point,angle,l)
         self.type='Dart'
-        self.L = l
-        a1 = (np.pi/180.)*36.
-        phi = l*(1.+np.sqrt(5))/2.
-
-        xs = [0,l*np.cos(a1),l*l/phi,l*np.cos(a1),0]
-        ys = [0,l*np.sin(a1),0,-l*np.sin(a1),0]
-
-        rmat = np.array([
-            [np.cos(angle),(-1.)*np.sin(angle)],
-            [np.sin(angle),np.cos(angle)]
-        ])
-
-        vs = np.vstack((xs,ys))
-        vs = np.dot(vs.T,rmat).T
-        vs[0]+=point[0]
-        vs[1]+=point[1]
-        self.verts = [(vs[0][i],vs[1][i]) for i in range(len(xs))]
-        self.angle = angle
-        self.a1 = a1
-        self.phi = phi
-
-        self.codes = [
-            Path.MOVETO,
-            Path.LINETO,
-            Path.LINETO,
-            Path.LINETO,
-            Path.CLOSEPOLY,
-        ]
-
-        self.path = Path(self.verts,self.codes)
+        self.xs = [0,l*np.cos(self.a1),l*l/self.phi,l*np.cos(self.a1),0]
+        self.ys = [0,l*np.sin(self.a1),0,-l*np.sin(self.a1),0]
+        self._make_path()
 
     def inflate(self):
         l = self.L - self.phi
@@ -89,13 +68,12 @@ class Dart():
         d2 = Dart(self.verts[3],self.angle+self.a1,l)
         return k,d1,d2
 
-        
 
 if __name__ == '__main__':
 
     tiles = [Dart([0,0],-1.2*np.pi/2,1)]
 
-    while len(tiles) < 5000:
+    while len(tiles) < 20000:
         tmt = []
         for t in tiles: tmt+=list(t.inflate())
         tiles = tmt
@@ -104,9 +82,9 @@ if __name__ == '__main__':
     ax = F.add_subplot(111)
     ax.set_aspect('equal')
     for t in tiles:
-        c = 'tab:blue' if t.type == 'Kite' else 'tab:purple'
-        p = patches.PathPatch(t.path,fc=c,ec='k')
+        cs = ['tab:blue','royalblue','dodgerblue'] if t.type == 'Kite' else ['tab:orange','darkorange','orange']
+        p = patches.PathPatch(t.path,fc=np.random.choice(cs),ec='k')
         ax.add_patch(p)
-    ax.set_xlim(-.75,.5)
-    ax.set_ylim(0,1)
+    ax.set_xlim(-.52,.11)
+    ax.set_ylim(.232,.65)
     plt.show()
